2550f7b4a1900b087f66ee852eb1702c0278e57a
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-08 09:34:10 -0500

    Generate a list of 10 random words

--- exercise.rb
+++ exercise.rb
@@ -1,3 +1,12 @@
 require 'HTTParty'
 
 response = HTTParty.get('http://setgetgo.com/randomword/get.php')
+
+words = []
+k = 1
+10.times do |k, v|
+  words[k] = HTTParty.get('http://setgetgo.com/randomword/get.php')
+  k += 1
+end
+
+puts words
